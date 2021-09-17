d756a094e8946d0753e04d0254fb2f7d58e22d9d
spencerf2 <spencer.franklin@hotmail.com>
2021-09-17 16:10:11 +0200

    Changed local database variable name to hosted variable name.

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config():
     FLASK_APP = os.environ.get('FLASK_APP')
     FLASK_ENV = os.environ.get('FLASK_ENV')
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEPLOY_DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False # Turn off SQLAlchemy update
                                            # messages
